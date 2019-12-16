f825b606d5bb316080735ae4c643d5d34d7e0f9d
Your Name <you@example.com>
2019-12-16 15:43:48 +0100

    add user creation and login form

---
+++ src/Validators/UserValidator.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Validators;
+
+use Symfony\Component\Console\Exception\InvalidArgumentException;
+
+
+class UserValidator
+{
+
+    public function validatePassword(?string $plainPassword): string
+    {
+        if (empty($plainPassword)) {
+            throw new InvalidArgumentException('The password can not be empty.');
+        }
+
+        if (mb_strlen(trim($plainPassword)) < 6) {
+            throw new InvalidArgumentException('The password must be at least 6 characters long.');
+        }
+
+        return $plainPassword;
+    }
+
+    public function validateEmail(?string $email): string
+    {
+        if (empty($email)) {
+            throw new InvalidArgumentException('The email can not be empty.');
+        }
+
+        if (false === mb_strpos($email, '@')) {
+            throw new InvalidArgumentException('The email should look like a real email.');
+        }
+
+        return $email;
+    }
+}
